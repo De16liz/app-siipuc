001a2cf8347ee137b8dbdc0f5b7afd75d52faba4
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2022-06-05 16:50:22 -0500

    Corrección slider

--- src/Views/Pages/inicio.view.php
+++ src/Views/Pages/inicio.view.php
@@ -1,7 +1,28 @@
 <?php use App\app; use App\Classes\inicio; ?>
 <div class="contenido-inicio">
     <div class="slider">
-
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" style="height: 400px;">
+                <div class="carousel-item active" data-bs-interval="2000">
+                    <img src="public/img/inicio/001.jpg" class="d-block w-100" alt="baner 1">
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="public/img/inicio/002.jpeg" class="d-block w-100" alt="baner 2">
+                </div>
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="public/img/inicio/003.jpeg" class="d-block w-100" alt="baner 3">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
     </div>
 </div>
 </div>
